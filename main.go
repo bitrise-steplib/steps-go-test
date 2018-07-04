ea6ff9affa43313940fabdcee882bdc917e06b29
Krisztián  Gödrei <godreikrisztian@gmail.com>
2018-07-04 16:55:03 +0200

    generate code coverage report

--- main.go
+++ main.go
@@ -2,18 +2,14 @@ package main
 
 import (
 	"os"
-	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/fileutil"
 	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/pathutil"
 )
-
-func installedInPath(name string) bool {
-	cmd := exec.Command("which", name)
-	outBytes, err := cmd.Output()
-	return err == nil && strings.TrimSpace(string(outBytes)) != ""
-}
 
 func failf(format string, args ...interface{}) {
 	log.Errorf(format, args...)
@@ -22,9 +18,11 @@ func failf(format string, args ...interface{}) {
 
 func main() {
 	packages := os.Getenv("packages")
+	codeCoveragePath := os.Getenv("code_coverage_path")
 
 	log.Infof("Configs:")
 	log.Printf("- packages: %s", packages)
+	log.Printf("- code_coverage_path: %s", codeCoveragePath)
 
 	if packages == "" {
 		failf("Required input not defined: packages")
@@ -32,13 +30,45 @@ func main() {
 
 	log.Infof("\nRunning go test...")
 
+	args := []string{"test", "-v"}
+
+	var packageCoverProfilePth string
+	if codeCoveragePath != "" {
+		if err := os.MkdirAll(filepath.Dir(codeCoveragePath), 0777); err != nil {
+			failf("Failed to make dir: %s", err)
+		}
+
+		tmpDir, err := pathutil.NormalizedOSTempDirPath("go-test")
+		if err != nil {
+			failf("Failed to create tmp dir: %s", err)
+		}
+		packageCoverProfilePth = filepath.Join(tmpDir, "profile.out")
+
+		args = append(args, "-race", "-coverprofile="+packageCoverProfilePth, "-covermode=atomic")
+	}
+
 	for _, p := range strings.Split(packages, "\n") {
-		cmd := command.NewWithStandardOuts("go", "test", "-v", p)
+		cmd := command.NewWithStandardOuts("go", append(args, p)...)
 
 		log.Printf("$ %s", cmd.PrintableCommandArgs())
 
 		if err := cmd.Run(); err != nil {
 			failf("go test failed: %s", err)
+		}
+
+		if codeCoveragePath != "" {
+			coverProfileContent, err := fileutil.ReadStringFromFile(packageCoverProfilePth)
+			if err != nil {
+				failf("Failed to read package cover profile: %s", err)
+			}
+
+			if err := fileutil.AppendStringToFile(codeCoveragePath, coverProfileContent); err != nil {
+				failf("Failed to append package cover profile: %s", err)
+			}
+
+			if err := os.RemoveAll(packageCoverProfilePth); err != nil {
+				failf("Failed to append package cover profile: %s", err)
+			}
 		}
 	}
 }
